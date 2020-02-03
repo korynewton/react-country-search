f176810642130a6463bfcc50d09803d00d27ea46
Kory Newton <korybnewton@gmail.com>
2020-02-03 13:53:14 -0800

    add style guide variables

---
+++ src/styles/variables.js
@@ -0,0 +1,15 @@
+// colors
+export const white = 'hsl(0,0%,100%);';
+export const veryLightGrayBackground = 'hsl(0, 0%, 98%);';
+export const darkGray = 'hsl(0,0%, 52%);';
+export const veryDarkBlueText = 'hsl(200, 15%, 8%);';
+export const veryDarkBlueBackground = 'hsl(207, 26%, 17%);';
+export const darkBlue = 'hsl(209, 23%, 22%);';
+
+// font details
+export const nunitoSansFont = '"Nunito Sans", sans-serif;';
+export const detailPageFontSize = '16px;';
+export const homePageFontSize = '14px;';
+export const fontWeightLight = '300;';
+export const fontWeightMedium = '600;';
+export const fontWeightHeavy = '800;';
