636327f48aefb472dc92f27109ce83a879db1aa1
Kory Newton <korybnewton@gmail.com>
2020-02-04 15:51:49 -0800

    remove border

--- src/styles/Main/Filter/FilterContainer.styles.js
+++ src/styles/Main/Filter/FilterContainer.styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const FilterContainer = styled.div`
     display flex;
     flex-direction: column;
-    border: 1px solid red;
     max-width: 91%;
     margin: 0 auto;
 `;
