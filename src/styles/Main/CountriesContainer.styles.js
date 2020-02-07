5a5496aa3646edeab331813e3f232229e1d746ee
Kory Newton <korybnewton@gmail.com>
2020-02-07 12:14:21 -0800

    fix z-index difference between filter component and countries container

--- src/styles/Main/CountriesContainer.styles.js
+++ src/styles/Main/CountriesContainer.styles.js
@@ -5,6 +5,8 @@ const CountriesContainer = styled.div`
   flex-direction: column;
   align-items: center;
   margin-top: 5.5rem;
+  position: relative:
+  z-index: 0;
 `;
 
 export default CountriesContainer;
--- src/styles/Main/Filter/FilterContainer.styles.js
+++ src/styles/Main/Filter/FilterContainer.styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 const FilterContainer = styled.div`
+    position: relative;
+    z-index: 1;
     display flex;
     flex-direction: column;
     max-width: 91%;
     margin: 0 auto;
+    height: 29rem;
 `;
 
 export default FilterContainer;
