4024a9a101b9a089a6c6eed044ff47b52ee4598f
Kory Newton <korybnewton@gmail.com>
2020-02-10 10:08:17 -0800

    add useEffect hook to scroll the window back to the top whenever the detailed country view is rendered

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { DetailsContainer } from '../styles/Main/Details/Details.styles';
 import DetailsComponent from '../components/Details/Details.component';
 import BorderCountriesComponent from '../components/Details/BorderCountries.component';
 
 const DetailsPage = ({ countryDetails, borderCountries, ...rest }) => {
+  useEffect(() => window.scrollTo(0, 0));
+
   return (
     <>
       <DetailsContainer>
